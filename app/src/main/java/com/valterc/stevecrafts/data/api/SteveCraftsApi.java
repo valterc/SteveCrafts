ddaa48cb58c1182a239b2e2d0106eecac4002332
Valter Costa <costinha_26@hotmail.com>
2015-01-13 00:42:34 +0000

    Work on the back end API;

--- app/src/main/java/com/valterc/stevecrafts/data/api/SteveCraftsApi.java
+++ app/src/main/java/com/valterc/stevecrafts/data/api/SteveCraftsApi.java
@@ -5,6 +5,10 @@ import android.graphics.Bitmap;
 import com.vcutils.DownloadResponse;
 import com.vcutils.Downloader;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.Date;
 
@@ -18,31 +22,49 @@ public class SteveCraftsApi {
     private static final String API_GET_NEW_DATA = "getNewData.php?key=%s&time=%s";
     private static final String API_GET_IMAGE = "getImage.php?key=%s&type=%s&id=%s";
 
-    public SteveCraftsApi(){
+    public SteveCraftsApi() {
 
     }
 
-    private String getKey(){
-        return "0";
+
+    public SteveCraftsData getData() throws JSONException {
+        DownloadResponse<String> response = Downloader.downloadGet(String.format(API_URL_BASE + API_GET_DATA, getKey()));
+        return parseData(response.getResponse());
     }
 
-    public void getData(){
-
-    }
-
-    public void getNewData(Date lastDataDate){
-
+    public SteveCraftsData getNewData(Date lastDataDate) throws JSONException {
+        DownloadResponse<String> response = Downloader.downloadGet(String.format(API_URL_BASE + API_GET_NEW_DATA, getKey(), lastDataDate.getTime()));
+        return parseData(response.getResponse());
     }
 
     public Bitmap getImage(String type, String id) throws IOException {
         DownloadResponse<Bitmap> response = Downloader.downloadImage(String.format(API_URL_BASE + API_GET_IMAGE, getKey(), type, id));
-        if (response.getResponse() != null){
+        if (response.getResponse() != null) {
             return response.getResponse();
         } else if (response.getStatusCode() == 404) {
             return null;
         } else {
             throw new IOException("Error downloading image.");
         }
+    }
+
+    private String getKey() {
+        return "0";
+    }
+
+    private SteveCraftsData parseData(String jsonResponse) throws JSONException {
+        JSONObject jsonBase = new JSONObject(jsonResponse);
+        JSONArray blocksArray = jsonBase.getJSONArray("blocks");
+        JSONArray breaksArray = jsonBase.getJSONArray("breaks");
+        JSONArray brewingArray = jsonBase.getJSONArray("brewing");
+        JSONArray craftingArray = jsonBase.getJSONArray("crafting");
+        JSONArray itemsArray = jsonBase.getJSONArray("items");
+        JSONArray potionsArray = jsonBase.getJSONArray("potions");
+        JSONArray smeltingArray = jsonBase.getJSONArray("smelting");
+
+
+
+        return null;
     }
 
 }
---
+++ app/src/main/java/com/valterc/stevecrafts/data/api/SteveCraftsData.java
@@ -0,0 +1,30 @@
+package com.valterc.stevecrafts.data.api;
+
+import com.valterc.stevecrafts.data.model.Block;
+import com.valterc.stevecrafts.data.model.Breaks;
+import com.valterc.stevecrafts.data.model.Brewing;
+import com.valterc.stevecrafts.data.model.CraftingRecipe;
+import com.valterc.stevecrafts.data.model.Item;
+import com.valterc.stevecrafts.data.model.Potion;
+import com.valterc.stevecrafts.data.model.Smelting;
+
+import java.util.List;
+
+/**
+ * Created by Valter on 12/01/2015.
+ */
+public class SteveCraftsData {
+
+    private List<Block> blocks;
+    private List<Breaks> breaks;
+    private List<Brewing> brewings;
+    private List<CraftingRecipe> craftingRecipes;
+    private List<Item> items;
+    private List<Potion> potions;
+    private List<Smelting> smeltings;
+
+    SteveCraftsData(){
+
+    }
+
+}
